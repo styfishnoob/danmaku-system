94adbb80607a1eae504b3f690ba1e5e9a9e0576c
styfishnoob <main.sakana@gmail.com>
2026-02-20 07:57:29 +0000

    feat: Selector に関する型を定義

---
+++ utils/types/selectors.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+import { createPlatformObject } from "@/utils/types/platforms";
+
+export const SelectorsSchema = z.object({
+  video: z.object({
+    canvas: createPlatformObject(z.string()),
+  }),
+
+  chat: z.object({
+    container: createPlatformObject(z.string()),
+    cell: createPlatformObject(z.string()),
+    contents: createPlatformObject(z.string()),
+    userName: createPlatformObject(z.string()),
+    messages: createPlatformObject(z.string()),
+    emotes: createPlatformObject(z.string()),
+  }),
+});
+
+export type Selectors = z.infer<typeof SelectorsSchema>;
